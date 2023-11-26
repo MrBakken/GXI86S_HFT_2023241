2206e68e4bef682bffb43df566a2338a7a33997d
Dominik Veres <veresdomi02@gmail.com>
2023-11-26 02:58:32 +0100

    Client/Endpoint 1/2

--- GXI86S_HFT_2023241.Client/Program.cs
+++ GXI86S_HFT_2023241.Client/Program.cs
@@ -1,116 +1,19 @@
 ﻿using ConsoleTools;
-using GXI86S_HFT_2023241.Logic;
 using GXI86S_HFT_2023241.Models;
-using GXI86S_HFT_2023241.Repository;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace GXI86S_HFT_2023241.Client
 {
     internal class Program
     {
-        static AccountLogic AccountLogic;
-        static CustomerLogic CustomerLogic;
-        static TransactionLogic TransactionLogic;
-
-        static void Create(string entity)
-        {
-            Console.WriteLine(entity + " create");
-            Console.ReadLine();
-        }
-        static void List(string entity)
-        {
-            switch (entity)
-            {
-                case "Customer":
-
-                    var cusitems = CustomerLogic.ReadAll();
-                    Console.WriteLine("{0,-10} |{1,-10} |{2,-24} |{3,-20} |{4,-16} |{5,-7}", "FirstName", "LastName", "Email", "Phone", "BirthDate", "Gender");
-                    Console.WriteLine(new string('-', 100));
-                    foreach (var item in cusitems)
-                    {
-                        Console.WriteLine("{0,-10} |{1,-10} |{2,-24} |{3,-20} |{4,-16} |{5,-7}", item.FirstName, item.LastName, item.Email, item.Phone, item.BirthDate.ToShortDateString(), item.Gender);
-                    }
-                    break;
-                case "Account":
-
-                    var Accitems = AccountLogic.ReadAll();
-                    Console.WriteLine("{0,-13} |{1,-20} |{2,-13} |{3,-11}", "AccountNum", "Balance(Currency)", "AccountType", "CreationDate");
-                    Console.WriteLine(new string('-', 100));
-                    foreach (var item in Accitems)
-                    {
-                        string InCurrencyType = item.Balance.ToString() + " " + item.CurrencyType;
-                        Console.WriteLine("{0,-13} |{1,-20} |{2,-13} |{3,-11}", item.AccountNumber_ID, InCurrencyType, item.AccountType, item.CreationDate.ToShortDateString());
-                    }
-                    break;
-                case "Transaction":
-                    var tranitems = TransactionLogic.ReadAll();
-                    Console.WriteLine("{0,-13} |{1,-30} |{2,-13} |{3,-30}", "AccountId", "Date", "Amount", "Description");
-                    Console.WriteLine(new string('-', 100));
-                    foreach (var item in tranitems)
-                    {
-                        Console.WriteLine("{0,-13} |{1,-30} |{2,-13} |{3,-30}", item.AccountId, item.Date, item.Amount, item.Description);
-                    }
-                    break;
-                default:
-                    break;
-            }
-            Console.ReadLine();
-        }
-        static void Update(string entity)           //Nem müködik
-        {
-            switch (entity)
-            {
-                case "Customer":
-                    Customer customer = new Customer();
-                    CustomerLogic.Update(customer);
-                    break;
-                case "Account":
-                    Account account = new Account();
-                    AccountLogic.Update(account);
-                    break;
-                case "Transaction":
-                    Transaction transaction = new Transaction();
-                    TransactionLogic.Update(transaction);
-                    break;
-                default:
-                    break;
-            }
-            Console.ReadLine();
-        }
-        static void Delete(string entity)
-        {
-            Console.WriteLine("Which ID item do you want to delete?\nÍrja ide: ");
-            int IDforDelete = int.Parse(Console.ReadLine());
-            switch (entity)
-            {
-                case "Customer":
-                    CustomerLogic.Delete(IDforDelete);
-                    break;
-                case "Account":
-                    AccountLogic.Delete(IDforDelete);
-                    break;
-                case "Transaction":
-                    TransactionLogic.Delete(IDforDelete);
-                    break;
-                default:
-                    break;
-            }
-            Console.ReadLine();
-        }
+        static RestService rest;
 
         static void Main(string[] args)
         {
-            var ctx = new BankDBContext();
-
-            var CustumerRepo = new CustomerRepository(ctx);
-            var AccRepo = new AccountRepository(ctx);
-            var TranRepo = new TransactionRepository(ctx);
-
-            CustomerLogic = new CustomerLogic(CustumerRepo);
-            AccountLogic = new AccountLogic(AccRepo);
-            TransactionLogic = new TransactionLogic(TranRepo, AccRepo);
+            rest =new RestService("http://localhost:34372", "account");
 
             var CustomerSubMenu = new ConsoleMenu(args, level: 1)
                 .Add("List", () => List("Customer"))
@@ -142,6 +45,135 @@ namespace GXI86S_HFT_2023241.Client
 
             menu.Show();
 
+        }
+
+        static void Create(string entity)       //befejezni
+        {
+            
+            
+            switch (entity)
+            {
+                case "Customer":
+                    Console.WriteLine("Enter a new Firstname\nWrite here: ");
+                    string newname = Console.ReadLine();
+                    rest.Post(new Customer() { FirstName = newname }, "customer");
+                    break;
+
+                case "Account":
+                    Console.WriteLine("Enter a new CurrencyType\nWrite here: ");
+                    CurrencyEnum NewAccCurrency = (CurrencyEnum)Enum.Parse(typeof(CurrencyEnum), Console.ReadLine());
+                    rest.Post(new Account() { CreationDate = DateTime.Now, CurrencyType = NewAccCurrency }, "account");
+                    break;
+
+                case "Transaction":
+                    Console.WriteLine("Enter an Amount\nWrite here: ");
+                    int newAmount = int.Parse(Console.ReadLine());
+                    rest.Post(new Transaction() { Amount = newAmount,Date = DateTime.Now }, "transaction");
+                    break;
+
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    
+        static void List(string entity)         //befejezni
+        {
+            switch (entity)
+            {
+                case "Customer":
+                    List<Customer> Customers = rest.Get<Customer>("customer");
+                    Console.WriteLine("{0,-10} |{1,-10} |{2,-24} |{3,-20} |{4,-16} |{5,-7}", "FirstName", "LastName", "Email", "Phone", "BirthDate", "Gender");
+                    Console.WriteLine(new string('-', 100));
+                    foreach (var item in Customers)
+                    {
+                        Console.WriteLine("{0,-10} |{1,-10} |{2,-24} |{3,-20} |{4,-16} |{5,-7}", item.FirstName, item.LastName, item.Email, item.Phone, item.BirthDate.ToShortDateString(), item.Gender);
+                    }
+                    break;
+
+                case "Account":
+                    List<Account> Accounts = rest.Get<Account>("account");
+                    Console.WriteLine("{0,-13} |{1,-20} |{2,-13} |{3,-11}", "AccountNum", "Balance(Currency)", "AccountType", "CreationDate");
+                    Console.WriteLine(new string('-', 100));
+                    foreach (var item in Accounts)
+                    {
+                        string InCurrencyType = item.Balance.ToString() + " " + item.CurrencyType;
+                        Console.WriteLine("{0,-13} |{1,-20} |{2,-13} |{3,-11}", item.AccountNumber_ID, InCurrencyType, item.AccountType, item.CreationDate.ToShortDateString());
+                    }
+                    break;
+
+                case "Transaction":
+                    List<Transaction> Transactions = rest.Get<Transaction>("transaction");
+                    Console.WriteLine("{0,-13} |{1,-30} |{2,-13} |{3,-30}", "AccountId", "Date", "Amount", "Description");
+                    Console.WriteLine(new string('-', 100));
+                    foreach (var item in Transactions)
+                    {
+                        Console.WriteLine("{0,-13} |{1,-30} |{2,-13} |{3,-30}", item.AccountId, item.Date, item.Amount, item.Description);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)               //befejezni
+        {
+            switch (entity)
+            {
+                case "Customer":
+                    Console.Write("Enter Customer's id to update: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Customer one = rest.Get<Customer>(id, "customer");
+                    Console.Write($"New name [old: {one.LastName}]: ");
+                    string name = Console.ReadLine();
+                    one.LastName = name;
+                    rest.Put(one, "actor");
+                    break;
+
+                case "Account":
+                    Console.Write("Enter Account's id to update: ");
+                    int accid = int.Parse(Console.ReadLine());
+                    Account accone = rest.Get<Account>(accid, "account");
+                    Console.Write($"New accountType [old: {accone.AccountType}] Write[Current/Savings]: ");
+                    AccountTypeEnum acctype = (AccountTypeEnum)Enum.Parse(typeof(AccountTypeEnum), Console.ReadLine());
+                    accone.AccountType = acctype;
+                    rest.Put(accone, "actor");
+                    break;
+
+                case "Transaction":
+                    Console.Write("Enter Transaction's id to update: ");
+                    int tranid = int.Parse(Console.ReadLine());
+                    Transaction tranone = rest.Get<Transaction>(tranid, "transaction");
+                    Console.Write($"New description [old: {tranone.Description}]: ");
+                    string newDescription = Console.ReadLine();
+                    tranone.Description = newDescription;
+                    rest.Put(tranone, "actor");
+                    break;
+
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void Delete(string entity)   //befejezni
+        {
+            Console.WriteLine($"Which {entity} ID item do you want to delete?\nWrite here: ");
+            int id = int.Parse(Console.ReadLine());
+            switch (entity)
+            {
+                case "Customer":
+                    rest.Delete(id, "customer");
+                    break;
+                case "Account":
+                    rest.Delete(id, "account");
+                    break;
+                case "Transaction":
+                    rest.Delete(id, "transaction");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
--- GXI86S_HFT_2023241.Repository/Database/BankDBContext.cs
+++ GXI86S_HFT_2023241.Repository/Database/BankDBContext.cs
@@ -36,24 +36,28 @@ namespace GXI86S_HFT_2023241.Repository
                 entity.Property(e => e.Email).HasMaxLength(240);
                 entity.Property(e => e.Phone).HasMaxLength(240);
                 entity.Property(e => e.BirthDate);
-                entity.Property(e => e.Gender);
+                entity.Property(e => e.Gender).HasConversion<string>();
 
-                entity.HasMany(c => c.Accounts) // Customer can have many accounts
-                        .WithOne(a => a.Customer) // Account belongs to one customer
-                        .HasForeignKey(a => a.CustomerId); // Foreign key in Account table
+                entity.HasMany(c => c.Accounts)
+                        .WithOne(a => a.Customer)
+                        .HasForeignKey(a => a.CustomerId);
             });
 
             modelBuilder.Entity<Account>(entity =>
             {
                 entity.HasKey(e => e.AccountNumber_ID);
                 entity.Property(e => e.CurrencyType).IsRequired().HasMaxLength(240);
-                entity.Property(e => e.Balance);
+                entity.Property(e => e.Balance).HasConversion<string>();
                 entity.Property(e => e.CreationDate);
-                entity.Property(e => e.AccountType);
+                entity.Property(e => e.AccountType).HasConversion<string>();
 
-                entity.HasMany(c => c.Transactions) // Customer can have many accounts
-                        .WithOne(a => a.Account) // Account belongs to one customer
-                        .HasForeignKey(a => a.AccountId); // Foreign key in Account table
+                entity.HasMany(c => c.Transactions)
+                        .WithOne(a => a.Account) 
+                        .HasForeignKey(a => a.AccountId);
+
+                entity.HasOne(t => t.Customer)
+                    .WithMany(a => a.Accounts)
+                    .HasForeignKey(t => t.CustomerId).OnDelete(DeleteBehavior.Cascade);
             });
 
             modelBuilder.Entity<Transaction>(entity =>
@@ -66,7 +70,7 @@ namespace GXI86S_HFT_2023241.Repository
 
                 entity.HasOne(t => t.Account)
                     .WithMany(a => a.Transactions)
-                    .HasForeignKey(t => t.AccountId);
+                    .HasForeignKey(t => t.AccountId).OnDelete(DeleteBehavior.Cascade);
             });
 
             modelBuilder.Entity<Customer>().HasData(
@@ -84,13 +88,13 @@ namespace GXI86S_HFT_2023241.Repository
 
             modelBuilder.Entity<Customer>().HasData(new Customer[]
                 {
-            new Customer("2$John$Doe$john@example.com$123-456-7890$1990-01-01$Male"),
+            new ("2$John$Doe$john@example.com$123-456-7890$1990-01-01$Male"),
 
-            new Customer("3$Michael$Johnson$michael@example.com$555-555-5555$1982-07-20$Male"),
+            new ("3$Michael$Johnson$michael@example.com$555-555-5555$1982-07-20$Male"),
 
-            new Customer("4$Emily$Brown$emily@example.com$444-333-2222$1995-05-10$Female"),
+            new ("4$Emily$Brown$emily@example.com$444-333-2222$1995-05-10$Female"),
 
-            new Customer("5$Annie$Taylor$Annie@example.com$777-888-9999$1988-11-30$Female"),
+            new ("5$Annie$Taylor$Annie@example.com$777-888-9999$1988-11-30$Female"),
                     });
 
 
@@ -108,18 +112,18 @@ namespace GXI86S_HFT_2023241.Repository
 
             modelBuilder.Entity<Account>().HasData(new Account[]
                 {
-                    new Account("1325345$1$HUF$62424$2011.04.02$Current"),
-                    new Account("0131314$1$EUR$4143431$2003.05.12$Savings"),
-                    new Account("0414424$2$HUF$21414$2014.05.19$Current"),
-                    new Account("9238446$2$EUR$14235$2023.05.18$Savings"),
-                    new Account("6352719$3$HUF$-43$2014.02.16$Current"),
-                    new Account("7458398$3$HUF$142545$2013.05.15$Savings"),
-                    new Account("3467425$4$EUR$2425636$2011.08.28$Current"),
-                    new Account("0941567$4$HUF$142356$2011.07.14$Savings"),
-                    new Account("1014251$4$HUF$14256346$2016.03.19$Savings"),
-                    new Account("1679531$5$EUR$13563$2011.07.15$Current"),
-                    new Account("1224551$5$HUF$746321$2012.02.20$Current"),
-                    new Account("1352566$5$HUF$246242$2017.05.1$Savings"),
+                    new ("1325345$1$HUF$62424$2011.04.02$Current"),
+                    new ("0131314$1$EUR$4143431$2003.05.12$Savings"),
+                    new ("0414424$2$HUF$21414$2014.05.19$Current"),
+                    new ("9238446$2$EUR$14235$2023.05.18$Savings"),
+                    new ("6352719$3$HUF$-43$2014.02.16$Current"),
+                    new ("7458398$3$HUF$142545$2013.05.15$Savings"),
+                    new ("3467425$4$EUR$2425636$2011.08.28$Current"),
+                    new ("0941567$4$HUF$142356$2011.07.14$Savings"),
+                    new ("1014251$4$HUF$14256346$2016.03.19$Savings"),
+                    new ("1679531$5$EUR$13563$2011.07.15$Current"),
+                    new ("1224551$5$HUF$746321$2012.02.20$Current"),
+                    new ("1352566$5$HUF$246242$2017.05.1$Savings"),
                     });
 
 
@@ -136,34 +140,34 @@ namespace GXI86S_HFT_2023241.Repository
 
             modelBuilder.Entity<Transaction>().HasData(new Transaction[]
                 {
-                    new Transaction("02$1325313$2023.02.28 14:45:20$-123$Uzsonna"),
-                    new Transaction("03$1325313$2023.01.15 08:30:00$14254$Fizetés"),
+                    new ("02$1325313$2023.02.28 14:45:20$-123$Uzsonna"),
+                    new ("03$1325313$2023.01.15 08:30:00$14254$Fizetés"),
 
-                    new Transaction("04$0131314$2023.03.10 18:00:45$15334$EURnics"),
-                    new Transaction("05$0131314$2023.04.05 09:15:30$-153$"),
+                    new ("04$0131314$2023.03.10 18:00:45$15334$EURnics"),
+                    new ("05$0131314$2023.04.05 09:15:30$-153$"),
 
-                    new Transaction("06$0414424$2023.05.20 12:00:00$145425$Felújítás"),
-                    new Transaction("07$0414424$2023.06.08 16:30:15$-1334254$"),
+                    new ("06$0414424$2023.05.20 12:00:00$145425$Felújítás"),
+                    new ("07$0414424$2023.06.08 16:30:15$-1334254$"),
 
-                    new Transaction("08$9238446$2023.07.12 21:45:55$14234$Autó"),
+                    new ("08$9238446$2023.07.12 21:45:55$14234$Autó"),
 
-                    new Transaction("09$6352719$2023.08.29 10:10:05$-142354$"),
+                    new ("09$6352719$2023.08.29 10:10:05$-142354$"),
 
-                    new Transaction("10$7458398$2023.09.14 13:20:40$13414$Szerelő"),
-                    new Transaction("11$7458398$2023.10.22 17:00:25$154235$Családnak"),
-                    new Transaction("12$7458398$2023.11.09 08:55:50$-1143$Asztal"),
+                    new ("10$7458398$2023.09.14 13:20:40$13414$Szerelő"),
+                    new ("11$7458398$2023.10.22 17:00:25$154235$Családnak"),
+                    new ("12$7458398$2023.11.09 08:55:50$-1143$Asztal"),
 
-                    new Transaction("13$3467425$2023.12.18 11:11:11$153365$Business"),
+                    new ("13$3467425$2023.12.18 11:11:11$153365$Business"),
 
-                    new Transaction("14$0941567$2022.01.05 10:15:30$6235414$Tuti"),
-                    new Transaction("15$0941567$2022.02.18 14:30:45$-51556$Tippmix"),
-                    new Transaction("16$0941567$2022.03.20 18:45:15$72413$Szárazzsemle"),
+                    new ("14$0941567$2022.01.05 10:15:30$6235414$Tuti"),
+                    new ("15$0941567$2022.02.18 14:30:45$-51556$Tippmix"),
+                    new ("16$0941567$2022.03.20 18:45:15$72413$Szárazzsemle"),
 
-                    new Transaction("17$1014251$2022.04.08 09:00:30$16345$"),
+                    new ("17$1014251$2022.04.08 09:00:30$16345$"),
 
-                    new Transaction("18$1679531$2022.05.15 12:30:00$-62626$valami"),
-                    new Transaction("19$1679531$2022.06.28 16:15:20$-142524$"),
-                    new Transaction("20$1679531$2023.08.29 10:10:05$1454$ruha"),
+                    new ("18$1679531$2022.05.15 12:30:00$-62626$valami"),
+                    new ("19$1679531$2022.06.28 16:15:20$-142524$"),
+                    new ("20$1679531$2023.08.29 10:10:05$1454$ruha"),
 
                     new Transaction("21$1224551$2022.07.10 21:00:55$-41425$haszontalanság"),
                     new Transaction("22$1224551$2023.05.20 12:00:00$14352$Roller"),
